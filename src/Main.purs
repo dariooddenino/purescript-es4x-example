4d96db9326da224efa387d3bf68c2ff76c034aea
Dario Oddenino <branch13@gmail.com>
2021-02-10 10:18:20 +0100

    wip

--- src/Main.purs
+++ src/Main.purs
@@ -5,8 +5,6 @@ import Es4x.Web
 import Es4x.Web.Template.Handlebars
 import Prelude
 
-import Control.Bind (bind, discard)
-import Data.Unit (Unit)
 import Effect (Effect)
 import Effect.Aff (Aff, Milliseconds(..), delay, launchAff_)
 import Effect.Aff.Class (class MonadAff, liftAff)
@@ -18,10 +16,9 @@ import Types (App(..), Config(..))
 
 
 
-
-
 createConfig :: Aff Config
 createConfig = do
+  -- create db pools, etc.
   pure $ Config { port: 8888 }
 
 runApp :: forall a. App a -> Effect Unit
@@ -33,7 +30,6 @@ runHandler :: forall a. Config -> App a -> Effect Unit
 runHandler config (App readerT) = do
   launchAff_ $ runReaderT readerT config
 
--- TODO: return a way to close the server?
 runHttpServer :: Router -> App Unit
 runHttpServer router = do
   config <- ask
@@ -59,12 +55,10 @@ instance templateRespondable
          => Respondable (Template r) where
   respond resp (Template path vals) = do
     liftEffect $ renderTemplate (writeJSON vals) path \tres -> do
-      -- TODO error handling
       resp' <- putHeader resp "Content-Type" "text/html; charset=UTF-8"
       endStr resp' (getTemplateResult tres)
 else
--- TODO: this right now works as a catch all, I think. I think it becomes a problem once more instances are being created?
--- I think we have to use Foreign or wrap in another data type. This also changes how handler should be written.
+-- This won't work, but it's enough for now
 instance jsonRespondable
         :: WriteForeign a
         => Respondable a where
@@ -73,19 +67,12 @@ instance jsonRespondable
     endStr resp' $ writeJSON a
 
 
--- data Foo = Foo Int
--- instance fooRespondable :: Respondable foo where
---   respond resp a = liftEffect $ endStr resp "Foo"
-
 createRoute' :: Router -> RoutePath -> App Route
 createRoute' router path = liftEffect $ createRoute router path
 
--- TODO I think we should pass the response to the handler, or give some
---      implicit way to handle it
 handler :: forall a. Respondable a => Router -> RoutePath -> (RoutingContext -> App a) -> App Unit
 handler router path ihandler = do
   iroute <- createRoute' router path
-  -- iroute <- lift $ liftEffect $ createRoute router path
   config <- ask
   liftEffect $ handle iroute \ctx -> do
     resp <- response ctx
@@ -96,7 +83,6 @@ handler router path ihandler = do
 stringHandler :: Router -> RoutePath -> (RoutingContext -> App String) -> App Unit
 stringHandler = handler
 
--- TODO we could "hardcode" or put in the reader the templates path and type?
 templateHandler :: forall r t r2. RowToList r t => WriteForeignFields t r () r2 => Router -> RoutePath -> (RoutingContext -> App (Template r)) -> App Unit
 templateHandler = handler
 
@@ -112,7 +98,7 @@ main = do
       pure "Hello String"
 
     templateHandler router "/fortunes" \req -> do
-      pure $ Template "templates/fortunes.hbs" { fortunes: [{ id: 1, message: "This template work" }] }
+      pure $ Template "templates/fortunes.hbs" { fortunes: [{ id: 1, message: "Hello Template" }] }
 
     jsonHandler router "/json" \req -> do
       pure { bananas: [1, 2, 3] }
